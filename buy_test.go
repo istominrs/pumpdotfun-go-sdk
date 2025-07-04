5f5cd64c8dd6319afeb48bc37b1acf928662cdd0
pumplord <pumplord67@gmail.com>
2025-07-04 10:26:24 +0300

    fix: interpret slippage as percent instead of basis points

--- buy_test.go
+++ buy_test.go
@@ -36,15 +36,15 @@ func TestCalculateBuyQuote(t *testing.T) {
 
 func TestConvertSlippageBasisPointsToPercentage(t *testing.T) {
 	t.Parallel()
-	
+
 	tests := []struct {
 		name                string
 		slippageBasisPoints uint
 		expected            float64
 	}{
 		{"Zero slippage", 0, 1.0},
-		{"Two percent slippage", 200, 0.98},
-		{"Full slippage", 10000, 0.0},
+		{"Two percent slippage", 2, 0.98},
+		{"Full slippage", 100, 0.0},
 	}
 
 	for _, tt := range tests {
